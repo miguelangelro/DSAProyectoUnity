8cf6ce9002b761a9e6c677f36bad08b4b3f7d439
Miguel Angel Rincon <miguel.angel.rincon@estudiantat.upc.edu>
2020-12-24 19:05:05 +0100

    Primer avance en los Mapas
    Hay un error a la hora de pintar el mapa pues lo dibuja de forma discontinua.
    
    Aparece el siguiente error:
    
    IndexOutOfRangeException: Index was outside the bounds of the array.
    BoardManager.SetupScene (System.String conjuntoMapa) (at Assets/Scripts/BoardManager.cs:72)
    GameManager.InitGame () (at Assets/Scripts/GameManager.cs:58)
    GameManager.Awake () (at Assets/Scripts/GameManager.cs:53)
    
    No veo porque está fuera de rango.

---
+++ JuegoDSA/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+    public GameObject acera;
+    public GameObject carretera;
+    public GameObject carreteraVertical;
+    public GameObject carreteraHorizontal;
+    public GameObject player;
+    public GameObject contorno;
+    public GameObject[] cespedTiles;
+    int xmapa;
+    int ymapa;
+    private Transform boardHolder;
+    private Transform boardHolder2;
+    //public int numMapa;
+
+
+    public void SetupScene(string conjuntoMapa) //paso el string con el diseño del mapa y info num filasxcolumnas y numero de nivel (se guarda en mapa), ejemplo: 25 25 1
+    {
+        string[] filas = conjuntoMapa.Split('\n');
+        int xtotal = Convert.ToInt32(filas[0].Split(' ')[0]);
+        int ytotal = Convert.ToInt32(filas[0].Split(' ')[1]);
+       // numMapa = Convert.ToInt32(filas[0].Split(' ')[2]);
+
+        int x;
+        int y;
+
+        boardHolder = new GameObject("Suelo").transform;
+        boardHolder2 = new GameObject("Contorno").transform;
+
+        //Ponemos una pared o el objeto que escojamos en el contorno del mapa (Lo mismo que los outerwalls)
+
+        for (x = -1; x < xtotal + 1; x++)
+        {
+            GameObject outerwall = Instantiate(contorno, new Vector3(x, -1f, 0f), Quaternion.identity);
+            outerwall.transform.SetParent(boardHolder2);
+        }
+
+        for (x = -1; x < xtotal + 1; x++)
+        {
+            GameObject outerwall = Instantiate(contorno, new Vector3(x, ytotal, 0f), Quaternion.identity);
+            outerwall.transform.SetParent(boardHolder2);
+        }
+
+        for (y = -1; y < ytotal + 1; y++)
+        {
+            GameObject outerwall = Instantiate(contorno, new Vector3(-1, y, 0f), Quaternion.identity);
+            outerwall.transform.SetParent(boardHolder2);
+        }
+
+        for (y = -1; y < ytotal + 1; y++)
+        {
+            GameObject outerwall = Instantiate(contorno, new Vector3(xtotal, y, 0f), Quaternion.identity);
+            outerwall.transform.SetParent(boardHolder2);
+        }
+
+
+        for (y = 0; y < ytotal; y++)
+        {
+            ymapa = -y + ytotal - 1; // invierto el sentido para poner bien en orden en el grid las cosas del mapa.
+            string linea = filas[y + 1];
+
+            for (x = 0; x < xtotal; x++)
+            {
+                xmapa = x; // las x no se invierten pues ya muestran el orden correcto.
+                char[] charArr = linea.ToCharArray();
+                char c = charArr[x];// miro que caracter hay en esa posicion.
+                CaseObject(c,xmapa,ymapa);
+            }
+        }
+
+    }
+
+        void CaseObject(char c,int xmapa, int ymapa) {
+
+        GameObject instance = null; //creo instancia para el suelo/cesped/carretera/acera ==> BoardHolder
+
+        switch (c)
+        {
+            case 'c':
+                    instance = Instantiate(carretera, new Vector3(xmapa, ymapa, 0f), Quaternion.identity);
+                break;
+
+            case 'v':
+                instance = Instantiate(carreteraVertical, new Vector3(xmapa, ymapa, 0f), Quaternion.identity);
+                break;
+
+            case 'h':
+                instance = Instantiate(carreteraHorizontal, new Vector3(xmapa, ymapa, 0f), Quaternion.identity);
+                break;
+
+            case 'a':
+                instance = Instantiate(acera, new Vector3(xmapa, ymapa, 0f), Quaternion.identity);
+                break;
+
+            default: //si hay espacio en blanco coloco cesped del tipo 0 o 1, aleatoriamente.
+                GameObject toInstantiate = cespedTiles[Random.Range(0, cespedTiles.Length)];
+                instance = Instantiate(toInstantiate, new Vector3(xmapa, ymapa, 0f), Quaternion.identity);
+                break;
+        }
+
+        if (instance != null)
+            instance.transform.SetParent(boardHolder); // asigno el tipo de suelo al BoardHolder
+
+    }
+
+}
---
+++ JuegoDSA/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+
+    public BoardManager boardScript;
+    public static GameManager instance = null;
+    string infoMapa;
+
+
+    public void Awake()
+    {
+       /* if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+       */
+        boardScript = GetComponent<BoardManager>();
+
+        this.infoMapa = "25 25                    \n" +
+                        "aaaaaaaaaaaaaaaaaaaaaaaaa\n" +
+                        "aaaaaaaaaaaaaaaaaaaaaaaaa\n" +
+                        "aacccccccccccccccccccccaa\n" +
+                        "aacvccccchhhhhhhcccccccaa\n" +
+                        "aacvcccccccccccccccccccaa\n" +
+                        "aacvc               cccaa\n" +
+                        "aacvc               cccaa\n" +
+                        "aaccc               cccaa\n" +
+                        "aaccc               cccaa\n" +
+                        "aaccc               cvcaa\n" +
+                        "aaccc               cvcaa\n" +
+                        "aacvc               cvcaa\n" +
+                        "aacvc               cvcaa\n" +
+                        "aacvc               cccaa\n" +
+                        "aacvc               cccaa\n" +
+                        "aaccc               cccaa\n" +
+                        "aaccc               cccaa\n" +
+                        "aaccc               cvcaa\n" +
+                        "aaccc               cvcaa\n" +
+                        "aaccc               cvcaa\n" +
+                        "aaccc               cvcaa\n" +
+                        "aacccccccccccccccccccccaa\n" +
+                        "aaccchhhhcccchhhhcccccaa\n" +
+                        "aacccccccccccccccccccccaa\n" +
+                        "aaaaaaaaaaaaaaaaaaaaaaaaa\n";
+
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(infoMapa);
+
+    }
+
+}
