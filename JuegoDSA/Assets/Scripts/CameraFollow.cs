3e02c12a931c568b4b5163ab94101fc32dddeb2f
Miguel Angel Rincon <miguel.angel.rincon@estudiantat.upc.edu>
2020-12-25 09:28:24 +0100

    Añadido camara de seguimiento a jugador

---
+++ JuegoDSA/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    void FixedUpdate() {
+
+        transform.position = new Vector2(player.position.x,player.position.y);
+    }
+}
