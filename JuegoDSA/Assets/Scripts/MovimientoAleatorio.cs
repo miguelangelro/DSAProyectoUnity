74be0e3185db71eb482cad5e6f703cb291dce384
carlosgm15 <carlos98gm@gmail.com>
2020-12-27 21:21:11 +0100

    scrip movimiento aleatorio
    creacion de un script para mover un objeto a spots aleatorios. He creado un objeto ciudadano pero unicamente para testear el script. Falta meterle un sprite, interacciones, etc

---
+++ JuegoDSA/Assets/Scripts/MovimientoAleatorio.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoAleatorio : MonoBehaviour
+{
+
+    public float speed; //marca la velocidad de movimiento
+    private float waitTime; //el tiempo que estara quieto una vez llegue al spot
+    public float startWaitTime;
+
+    public Transform moveSpot; //seran los sitios donde se moverá
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY; //marcan los limites de donde se  movera
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position,moveSpot.position) < 0.2f){
+            if(waitTime <= 0)
+            {
+                moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                waitTime = startWaitTime;
+
+            } else
+            {
+                waitTime -= Time.deltaTime;
+
+            }
+
+        }
+        
+    }
+}
